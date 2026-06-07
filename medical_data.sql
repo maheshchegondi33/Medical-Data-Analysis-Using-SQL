75cce0a540d08ed3e288b37c6476d30f090d4042
Mahesh <maheshchegondi33@gmail.com>
2026-06-07 23:58:13 +0530

    Update medical_data.sql

--- medical_data.sql
+++ medical_data.sql
@@ -1,5 +1,5 @@
 create database medical_data;
-use medical_data; mahi
+use medical_data; 
 
 -- 1. Show first name, last name, and gender of patients who's gender is 'M'
 select first_name,last_name,gender from patients where gender='M';
